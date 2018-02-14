f0ce2dc5a0955aa64be323c4a277146a60d2c8dc
Hansung Kim <gnidoc327@vinulabs.com>
2018-02-14 22:12:42 +0900

    add gugudan.

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+console.log("This is gugu class");
+
+
+for (var i = 1; i < 10; i++) {
+	for (var j = 1; j < 10; j++) {
+		console.log(i + " X " + j + " = " + i*j);
+	}
+}
